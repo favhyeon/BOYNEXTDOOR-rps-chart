0000cfd886fd94007c1c749d1d0c6cf2eeb6316a
favhyeon <favhyeon@gmail.com>
2026-08-12 13:01:30 +0900

    색상 커스텀 기능 추가

--- script.js
+++ script.js
@@ -47,10 +47,10 @@ const pairNames = [
 
 const options = [
     { name: "OTP",      color: "#f7cde0" },
-    { name: "좋아함",   color: "#f14d5e" },
+    { name: "좋아함",   color: "#ffafaf" },
     { name: "호감",     color: "#fcee90" },
-    { name: "관심있음", color: "#c0fdbf" },
-    { name: "관심없음", color: "#baebbb" },
+    { name: "관심있음", color: "#baebbb" },
+    { name: "관심없음", color: "#ffffff" },
     { name: "별로",     color: "#bfeefd" },
     { name: "지뢰",     color: "#999999" }
 ];
